fc0d271be59076257bf0e3af9a710e0c6ce15c50
Fırat Selim ZREKOLU <zirekoglufs@gmail.com>
2020-06-23 04:31:16 +0300

    created config type

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,11 @@
-import { Config } from "tombalaApi"
+import * as api from "tombalaApi"
+
+export interface Config extends api.Config {
+	gameIds: number[],
+}
 export default function cfg(): Promise<Config> {
 	return Promise.resolve({
-		apiAddr:"https://demoapi.liderbingo.com"
+		apiAddr: "https://demoapi.liderbingo.com",
+		gameIds: [1],
 	})
 }
